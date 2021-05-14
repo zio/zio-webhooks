6870c12728091e36208b4d56292ecc7da8d75a6c
Zach Albia <zach.albia@gmail.com>
2021-05-14 10:18:13 +0800

    add silencer comment

--- project/BuildHelper.scala
+++ project/BuildHelper.scala
@@ -4,8 +4,8 @@ import sbtbuildinfo._
 import BuildInfoKeys._
 
 object BuildHelper {
-  private val Scala212        = "2.12.12"
-  private val Scala213        = "2.13.3"
+  private val Scala212        = "2.12.12" // 2.12 & 2.13 versions are
+  private val Scala213        = "2.13.3"  // for silencer compatibility
   private val SilencerVersion = "1.7.1"
 
   private val stdOptions = Seq(
