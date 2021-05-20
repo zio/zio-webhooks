332d69d0f36386b4b87fed7d456baf50baf0c29a
Zach Albia <zach.albia@gmail.com>
2021-05-20 15:00:23 +0800

    add WebhookDeliveryMode values

--- webhooks/src/main/scala/zio/webhooks/WebhookDeliveryMode.scala
+++ webhooks/src/main/scala/zio/webhooks/WebhookDeliveryMode.scala
@@ -1,8 +1,15 @@
 package zio.webhooks
 
 /**
- * A [[WebhookDeliveryMode]] specifies two aspects of webhook delivery: [[WebhookDeliveryBatching]], whether the
- * delivery of events is done one-by-one or in batches; and [[WebhookDeliverySemantics]], which specify a delivery goal
- * of at least once, or at most once.
+ * A [[WebhookDeliveryMode]] specifies two aspects of webhook delivery: [[WebhookDeliveryBatching]],
+ * whether the delivery of events is done one-by-one or in batches; and
+ * [[WebhookDeliverySemantics]], which specify a delivery goal of at least once, or at most once.
  */
-final case class WebhookDeliveryMode(batching: WebhookDeliveryBatching, semantics: WebhookDeliverySemantics)
+final case class WebhookDeliveryMode private (batching: WebhookDeliveryBatching, semantics: WebhookDeliverySemantics)
+
+object WebhookDeliveryMode {
+  val BatchedAtLeastOnce = WebhookDeliveryMode(WebhookDeliveryBatching.Batched, WebhookDeliverySemantics.AtLeastOnce)
+  val BatchedAtMostOnce  = WebhookDeliveryMode(WebhookDeliveryBatching.Batched, WebhookDeliverySemantics.AtMostOnce)
+  val SingleAtLeastOnce  = WebhookDeliveryMode(WebhookDeliveryBatching.Single, WebhookDeliverySemantics.AtLeastOnce)
+  val SingleAtMostOnce   = WebhookDeliveryMode(WebhookDeliveryBatching.Single, WebhookDeliverySemantics.AtMostOnce)
+}
