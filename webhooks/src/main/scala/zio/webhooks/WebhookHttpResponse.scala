c666c11eb66426cd77ced6f920147a2f6138f9a7
Zach Albia <zach.albia@gmail.com>
2021-06-01 01:12:43 +0800

    refactor WebhookHttpResponse

--- webhooks/src/main/scala/zio/webhooks/WebhookHttpResponse.scala
+++ webhooks/src/main/scala/zio/webhooks/WebhookHttpResponse.scala
@@ -4,12 +4,8 @@ package zio.webhooks
  * A [[WebhookHttpResponse]] contains a `statusCode`: the only response data relevant to webhook
  * delivery.
  */
-final class WebhookHttpResponse private (val statusCode: Int) extends AnyVal {
+final case class WebhookHttpResponse(statusCode: Int) {
   def isSuccess: Boolean = statusCode == 200
 
   def isFailure: Boolean = !isSuccess
-}
-
-object WebhookHttpResponse {
-  def apply(statusCode: Int): WebhookHttpResponse = new WebhookHttpResponse(statusCode)
 }
