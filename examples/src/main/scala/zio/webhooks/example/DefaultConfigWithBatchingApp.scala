0be4b1bcc7a37620f8ce5772b70c1d1f45f32ff7
Zach Albia <zach.albia@gmail.com>
2021-06-15 12:53:29 +0800

    add default batching config example

--- examples/src/main/scala/zio/webhooks/example/DefaultConfigWithBatchingApp.scala
+++ examples/src/main/scala/zio/webhooks/example/DefaultConfigWithBatchingApp.scala
@@ -1,7 +1,61 @@
 package zio.webhooks.example
 
+import zhttp.http._
+import zhttp.service.Server
 import zio._
+import zio.console._
+import zio.duration._
+import zio.magic._
+import zio.stream.UStream
+import zio.webhooks._
+import zio.webhooks.backends.sttp.WebhookSttpClient
+import zio.webhooks.testkit._
 
 object DefaultConfigWithBatchingApp extends App {
-  def run(args: List[String]): URIO[zio.ZEnv, ExitCode] = ???
+
+  private val httpApp = HttpApp.collectM {
+    case request @ Method.POST -> Root / "endpoint" =>
+      ZIO
+        .foreach(request.getBodyAsString)(str => putStrLn(s"""SERVER RECEIVED PAYLOAD: "$str""""))
+        .as(Response.status(Status.OK))
+  }
+
+  private lazy val events = UStream.iterate(0L)(_ + 1).map { i =>
+    WebhookEvent(
+      WebhookEventKey(WebhookEventId(i), webhook.id),
+      WebhookEventStatus.New,
+      s"""{"payload":$i}""",
+      Chunk(("Accept", "*/*"), ("Content-Type", "application/json"))
+    )
+  }
+
+  private lazy val port = 8080
+
+  private def program =
+    for {
+      _ <- Server.start(port, httpApp).fork
+      _ <- WebhookServer.getErrors.use(UStream.fromQueue(_).map(_.toString).foreach(putStrLnErr(_))).fork
+      _ <- TestWebhookRepo.createWebhook(webhook)
+      _ <- events.schedule(Schedule.fixed(500.millis)).foreach(TestWebhookEventRepo.createEvent)
+    } yield ()
+
+  def run(args: List[String]): URIO[zio.ZEnv, ExitCode] =
+    program
+      .injectCustom(
+        TestWebhookRepo.test,
+        TestWebhookStateRepo.test,
+        TestWebhookEventRepo.test,
+        WebhookSttpClient.live,
+        WebhookServerConfig.defaultWithBatching,
+        WebhookServer.live
+      )
+      .exitCode
+
+  private lazy val webhook = Webhook(
+    id = WebhookId(0),
+    url = s"http://0.0.0.0:$port/endpoint",
+    label = "test webhook",
+    WebhookStatus.Enabled,
+    WebhookDeliveryMode.BatchedAtLeastOnce
+  )
 }
